a4766e3e793cb949f6a476071fdc7201ac1c92b1
Davide Esposito <davideesposito2911@gmail.com>
2024-02-21 16:22:49 +0100

    add interactive back-to-top-btn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Bookmarks-Button
 const bookmarkBtns = document.querySelectorAll(`.bookmark`);
 
 console.log(bookmarkBtns);
@@ -6,5 +7,17 @@ bookmarkBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         btn.classList.toggle("bookmarked");
         btn.textContent = btn.classList.contains('bookmarked') ? '✅' : '☑️';
+    });
+});
+
+// Back-to-Top-Button
+const backToTopBtn = document.querySelector(".back-to-top");
+
+console.log(backToTopBtn);
+
+backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
     });
 });
